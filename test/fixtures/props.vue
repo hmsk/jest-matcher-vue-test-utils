afb3ec4521dc0103e4a9243a993d74276ec08f0c
hmsk <k.hamasaki@gmail.com>
2018-08-19 03:50:04 -0700

    Be importable .vue in test

---
+++ test/fixtures/props.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="props">
+    Matchers for Prop
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      internal: "hi"
+    }
+  }
+}
+</script>
--- test/props.test.ts
+++ test/props.test.ts
@@ -1,7 +1,11 @@
 import toBeRequiredProp from "@/main";
+import { mount } from "@vue/test-utils";
+import Component from "./fixtures/props.vue";
 
 describe("props", () => {
   it("works", () => {
+    const wrapper = mount(Component);
+    expect(wrapper.vm.internal).toBe("hi");
     expect(toBeRequiredProp).toBeTruthy();
   });
 });
