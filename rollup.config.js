baf92d0eefd1388198bcb401658b87556e07850d
hmsk <k.hamasaki@gmail.com>
2018-08-21 02:36:54 -0700

    Setup Rollup config for releasing

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
 import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from 'rollup-plugin-json';
 
 export default {
   input: "./src/main.ts",
 
   plugins: [
-    typescript()
+    resolve(),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'node_modules/@vue/test-utils/dist/vue-test-utils.js': [ 'shallowMount' ]
+      }
+    }),
+    typescript(),
+    json()
   ],
 
   output: {
     file: "index.js",
     format: "cjs"
-  }
+  },
+
+  external: ["@vue/test-utils"]
 }
