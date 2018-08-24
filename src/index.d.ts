3d2b3856067cd5120d122fcab8bd902966c06db6
hmsk <k.hamasaki@gmail.com>
2018-08-23 23:55:08 -0700

    Next matcher ideas

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,8 @@
 declare namespace jest {
   interface Matchers<R> {
     toRequireProp (prop: string): R;
+    toTypeProp (prop: string, type: any | any[]): R;
+    toHaveDefaultProp (prop: string, defaultValue: any): R;
+    toClaimProp (prop: string, sampleValue: any): R;
   }
 }
