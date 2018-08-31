92497c110cc87ddfa50ad101e03cc5251cfdd2e9
hmsk <k.hamasaki@gmail.com>
2018-08-30 23:53:08 -0700

    Add general matchers

--- jest-matcher-vue-test-utils.d.ts
+++ jest-matcher-vue-test-utils.d.ts
@@ -3,6 +3,8 @@ declare namespace jest {
   interface Matchers<R> {
     toRequireProp (prop: string, options?: ComponentOptions): R;
     toHaveDefaultProp (prop: string, defaultValue: any, options?: ComponentOptions): R;
+    toBeValidProps (props: any, options?: ComponentOptions): R;
+    toBeValidProp (prop: string, sampleValue: any, options?: ComponentOptions): R;
     toBeValidPropWithTypeCheck (prop: string, type: any | any[], options?: ComponentOptions): R;
     toBeValidPropWithCustomValidator (prop: string, sampleValue: any, options?: ComponentOptions): R;
   }
