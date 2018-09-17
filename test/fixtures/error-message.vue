fa8f2cd89d8414e2911a9b039b82e738f79ca9ee
hmsk <k.hamasaki@gmail.com>
2018-09-17 00:40:28 -0700

    Create a new fixture SFC

---
+++ test/fixtures/error-message.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-if="isError" class="error">message</p>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: {
+    initialError: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: function () {
+    return {
+      isError: false
+    }
+  },
+  methods: {
+    created () {
+      this.isError = this.initialError;
+    },
+    showError () {
+      isError = true;
+    },
+    dismissError () {
+      isError = false;
+    }
+  }
+}
+</script>
