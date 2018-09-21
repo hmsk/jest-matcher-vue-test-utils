caa77b803e9c546710b0e4f83f6a7c9137a727d4
hmsk <k.hamasaki@gmail.com>
2018-09-20 23:51:25 -0700

    Define type of toAppear

--- jest-matcher-vue-test-utils.d.ts
+++ jest-matcher-vue-test-utils.d.ts
@@ -1,8 +1,12 @@
+import { Wrapper, NameSelector, RefSelector, VueClass } from "@vue/test-utils";
+import { FunctionalComponentOptions } from "vue";
+
 declare type ComponentOptions = import("./src/main").MatcherComponentOptions<import("vue").default>;
 declare type ComponentProp = import("./src/main").ComponentProp;
 
 declare namespace jest {
   interface Matchers<R> {
+    toAppear (prop: Wrapper<import("vue").default>, findAgrument: string | NameSelector | RefSelector | FunctionalComponentOptions | VueClass<import("vue").default> | ComponentOptions): R;
     toRequireProp (prop: string, options?: ComponentOptions): R;
     toHaveDefaultProp (prop: string, defaultValue: any, options?: ComponentOptions): R;
     toBeValidProps (props: ComponentProp, options?: ComponentOptions): R;
