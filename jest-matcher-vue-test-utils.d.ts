b0bf3586667e896c2ddd0ec8c646edfbfb616823
hmsk <k.hamasaki@gmail.com>
2018-09-24 19:54:15 -0700

    Give jest's global space for typings

--- jest-matcher-vue-test-utils.d.ts
+++ jest-matcher-vue-test-utils.d.ts
@@ -4,16 +4,18 @@ import { FunctionalComponentOptions } from "vue";
 declare type ComponentOptions = import("./src/main").MatcherComponentOptions<import("vue").default>;
 declare type ComponentProp = import("./src/main").ComponentProp;
 
-declare namespace jest {
-  interface Matchers<R> {
-    toAppear (prop: Wrapper<import("vue").default>, findAgrument: string | NameSelector | RefSelector | FunctionalComponentOptions | VueClass<import("vue").default> | ComponentOptions): R;
-    toDisappear (prop: Wrapper<import("vue").default>, findAgrument: string | NameSelector | RefSelector | FunctionalComponentOptions | VueClass<import("vue").default> | ComponentOptions): R;
-    toRequireProp (prop: string, options?: ComponentOptions): R;
-    toHaveDefaultProp (prop: string, defaultValue: any, options?: ComponentOptions): R;
-    toBeValidProps (props: ComponentProp, options?: ComponentOptions): R;
-    toBeValidProp (prop: string, sampleValue: any, options?: ComponentOptions): R;
-    toBeValidPropWithTypeCheck (prop: string, type: any | any[], options?: ComponentOptions): R;
-    toBeValidPropWithCustomValidator (prop: string, sampleValue: any, options?: ComponentOptions): R;
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toAppear (prop: Wrapper<import("vue").default>, findAgrument: string | NameSelector | RefSelector | FunctionalComponentOptions | VueClass<import("vue").default> | ComponentOptions): R;
+      toDisappear (prop: Wrapper<import("vue").default>, findAgrument: string | NameSelector | RefSelector | FunctionalComponentOptions | VueClass<import("vue").default> | ComponentOptions): R;
+      toRequireProp (prop: string, options?: ComponentOptions): R;
+      toHaveDefaultProp (prop: string, defaultValue: any, options?: ComponentOptions): R;
+      toBeValidProps (props: ComponentProp, options?: ComponentOptions): R;
+      toBeValidProp (prop: string, sampleValue: any, options?: ComponentOptions): R;
+      toBeValidPropWithTypeCheck (prop: string, type: any | any[], options?: ComponentOptions): R;
+      toBeValidPropWithCustomValidator (prop: string, sampleValue: any, options?: ComponentOptions): R;
+    }
   }
 }
 
