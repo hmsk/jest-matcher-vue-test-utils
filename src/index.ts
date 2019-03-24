647faedc3b51af06c1f8bf12e2ccd5aa9f3caec5
hmsk <k.hamasaki@gmail.com>
2019-03-24 01:59:33 -0700

    Separate toHaveDefaultProp

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,6 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       /**
-       * Asserts that the component gives default value for the prop
-       * @param {string} prop - The prop's name
-       * @param {any} defaultValue - The default value you're expecting for the prop
-       * @param options - Mount Option of the component
-       * @example
-       * expect(AComponent).toHaveDefaultProp("type", "I am a default message")
-       */
-      toHaveDefaultProp (prop: string, defaultValue: any, options?: MatcherComponentOptions<Vue>): R;
-      /**
        * Asserts that the component accepts the set of props
        * @param {any} props - The set of props
        * @param options - Mount Option of the component
@@ -106,32 +97,6 @@ export function toBeValidProp<V extends Vue> (
   };
 }
 
-export function toHaveDefaultProp<V extends Vue> (
-  received: MatcherComponent<V>,
-  propName: string,
-  defaultValue: any,
-  dynamicMountOptions?: MatcherComponentOptions<V>
-): MatcherResult {
-  const mountOption = dynamicMountOptions ?
-    overwriteConfiguration<V>({ mountOptions: dynamicMountOptions }).mountOptions :
-    getConfiguration<V>().mountOptions;
-
-  let wrapper;
-  withMockWarning(() => {
-    wrapper = shallowMount<V>(corkComponent(received), { ...mountOption });
-  });
-
-  const given = wrapper.props()[propName];
-  const matched = (this as jest.MatcherUtils).equals(given, defaultValue);
-
-  return {
-    message: matched ?
-      () => `'${propName}' prop is given '${defaultValue}' as default` :
-      () => `'${propName}' prop is not given '${defaultValue}' as default (is given '${given}')`,
-    pass: matched
-  };
-}
-
 export function toBeValidPropWithTypeCheck<V extends Vue> (
   received: MatcherComponent<V>,
   propName: string,
@@ -183,6 +148,7 @@ import toHaveEmitted from "./matchers/toHaveEmitted";
 import toDispatch from "./matchers/toDispatch";
 import toHaveDispatched from "./matchers/toHaveDispatched";
 import toRequireProp from "./matchers/toRequireProp";
+import toHaveDefaultProp from "./matchers/toHaveDefaultProp";
 
 import vuexPlugin from "./vuex-plugin";
 
@@ -194,6 +160,7 @@ export {
   toDispatch,
   toHaveDispatched,
   toRequireProp,
+  toHaveDefaultProp,
   vuexPlugin
 };
 
---
+++ src/matchers/toHaveDefaultProp.ts
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import { shallowMount } from "@vue/test-utils";
+import { MatcherResult, withMockWarning, corkComponent } from "../utils";
+import { overwriteConfiguration, getConfiguration } from "../config";
+import { MatcherComponent, MatcherComponentOptions } from "../";
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      /**
+       * Asserts that the component gives default value for the prop
+       * @param {string} prop - The prop's name
+       * @param {any} defaultValue - The default value you're expecting for the prop
+       * @param options - Mount Option of the component
+       * @example
+       * expect(AComponent).toHaveDefaultProp("type", "I am a default message")
+       */
+      toHaveDefaultProp (prop: string, defaultValue: any, options?: MatcherComponentOptions<Vue>): R;
+    }
+  }
+}
+
+export default function toHaveDefaultProp<V extends Vue> (
+  received: MatcherComponent<V>,
+  propName: string,
+  defaultValue: any,
+  dynamicMountOptions?: MatcherComponentOptions<V>
+): MatcherResult {
+  const mountOption = dynamicMountOptions ?
+    overwriteConfiguration<V>({ mountOptions: dynamicMountOptions }).mountOptions :
+    getConfiguration<V>().mountOptions;
+
+  let wrapper;
+  withMockWarning(() => {
+    wrapper = shallowMount<V>(corkComponent(received), { ...mountOption });
+  });
+
+  const given = wrapper.props()[propName];
+  const matched = (this as jest.MatcherUtils).equals(given, defaultValue);
+
+  return {
+    message: matched ?
+      () => `'${propName}' prop is given '${defaultValue}' as default` :
+      () => `'${propName}' prop is not given '${defaultValue}' as default (is given '${given}')`,
+    pass: matched
+  };
+}
