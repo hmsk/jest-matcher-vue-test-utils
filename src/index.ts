aa0e6200ff5ec3d583453bfcce081756351c66e2
hmsk <k.hamasaki@gmail.com>
2019-03-24 01:54:57 -0700

    Separate toRequireProp matcher

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,6 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       /**
-       * Asserts that the component requires the prop
-       * @param {string} prop - The prop's name
-       * @param options - Mount Option of the component
-       * @example
-       * expect(AComponent).toRequireProp("type")
-       */
-      toRequireProp (prop: string, options?: MatcherComponentOptions<Vue>): R;
-      /**
        * Asserts that the component gives default value for the prop
        * @param {string} prop - The prop's name
        * @param {any} defaultValue - The default value you're expecting for the prop
@@ -114,25 +106,6 @@ export function toBeValidProp<V extends Vue> (
   };
 }
 
-export function toRequireProp<V extends Vue> (
-  received: MatcherComponent<V>,
-  propName: string,
-  dynamicMountOptions?: MatcherComponentOptions<V>
-): MatcherResult {
-  const messages = getWarningsByMount(received, {}, dynamicMountOptions);
-
-  const found = messages.find((c) => {
-    return c.find((arg: string) => arg.includes(`Missing required prop: "${propName}"\n`))
-  });
-
-  return {
-    message: !!found ?
-      () => `'${propName}' prop is claimed as required` :
-      () => `'${propName}' prop is not claimed as required`,
-    pass: !!found
-  };
-}
-
 export function toHaveDefaultProp<V extends Vue> (
   received: MatcherComponent<V>,
   propName: string,
@@ -209,6 +182,7 @@ import toEmit from "./matchers/toEmit";
 import toHaveEmitted from "./matchers/toHaveEmitted";
 import toDispatch from "./matchers/toDispatch";
 import toHaveDispatched from "./matchers/toHaveDispatched";
+import toRequireProp from "./matchers/toRequireProp";
 
 import vuexPlugin from "./vuex-plugin";
 
@@ -219,6 +193,7 @@ export {
   toHaveEmitted,
   toDispatch,
   toHaveDispatched,
+  toRequireProp,
   vuexPlugin
 };
 
---
+++ src/matchers/toRequireProp.ts
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import { getWarningsByMount, MatcherResult } from "../utils";
+import { MatcherComponent, MatcherComponentOptions } from "../";
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      /**
+       * Asserts that the component requires the prop
+       * @param {string} prop - The prop's name
+       * @param options - Mount Option of the component
+       * @example
+       * expect(AComponent).toRequireProp("type")
+       */
+      toRequireProp (prop: string, options?: MatcherComponentOptions<Vue>): R;
+    }
+  }
+}
+
+export default function toRequireProp<V extends Vue> (
+  received: MatcherComponent<V>,
+  propName: string,
+  dynamicMountOptions?: MatcherComponentOptions<V>
+): MatcherResult {
+  const messages = getWarningsByMount(received, {}, dynamicMountOptions);
+
+  const found = messages.find((c) => {
+    return c.find((arg: string) => arg.includes(`Missing required prop: "${propName}"\n`))
+  });
+
+  return {
+    message: !!found ?
+      () => `'${propName}' prop is claimed as required` :
+      () => `'${propName}' prop is not claimed as required`,
+    pass: !!found
+  };
+}
